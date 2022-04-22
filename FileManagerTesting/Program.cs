d301e470939ab2544d05a659258502851de440a5
aaronb05 <aboyles05@gmail.com>
2022-04-22 11:19:34 -0400

    Add project files.

---
+++ FileManagerTesting/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManagerTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watcher = new FileSystemWatcher(@"C:\Users\aboyles\Documents\AaronB\Documentation");
+
+            watcher.NotifyFilter = NotifyFilters.Attributes |
+                                   NotifyFilters.CreationTime |
+                                   NotifyFilters.FileName |
+                                   NotifyFilters.LastAccess |
+                                   NotifyFilters.LastWrite |
+                                   NotifyFilters.Security |
+                                   NotifyFilters.Size;
+            watcher.Changed += OnChanged;
+            watcher.Created += OnCreated;
+            watcher.Deleted += OnDeleted;
+            watcher.Renamed += OnRenamed;
+            watcher.Error += OnError;
+            watcher.Filter = "*.txt";
+            watcher.IncludeSubdirectories = true;
+            watcher.EnableRaisingEvents = true;
+
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
+        }
+
+        public static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType != WatcherChangeTypes.Changed)
+            {
+                return;
+            }
+
+            string sourcePath = @"C:\Users\aboyles\Documents\AaronB\Documentation";
+            string targetPath = @"C:\Users\aboyles\Documents\AaronB\test";
+
+
+            if (Directory.Exists(targetPath))
+            {
+                string[] files = Directory.GetFiles(sourcePath);
+
+                foreach(string s in files)
+                {
+                    var fileName = Path.GetFileName(e.Name);
+                    var destFile = Path.Combine(targetPath, fileName);
+                    File.Copy(s, destFile, true);
+                }               
+            }
+
+            Console.WriteLine($"Changed: {e.FullPath}");
+            Console.WriteLine($"New Path: {targetPath}");
+        }
+
+        private static void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            string value = $"Created: {e.FullPath}";
+            Console.WriteLine(value);
+        }
+
+        private static void OnDeleted(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"Deleted: {e.FullPath}");
+        }
+
+        private static void OnRenamed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"Renamed:");
+            Console.WriteLine($"    Old: {e.FullPath}");
+            Console.WriteLine($"    New: {e.FullPath}");
+        }
+
+        private static void OnError(object sender, ErrorEventArgs e) =>
+           PrintException(e.GetException());
+
+        private static void PrintException(Exception ex)
+        {
+            if (ex != null)
+            {
+                Console.WriteLine($"Message: {ex.Message}");
+                Console.WriteLine("Stacktrace:");
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine();
+                PrintException(ex.InnerException);
+            }
+        }
+    }
+}
